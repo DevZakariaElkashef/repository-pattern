34090726ee03a28012809097e3dd55d9359bacfd
DevZakariaElkashef <dev.zakariaelkashef@gmail.com>
2023-08-25 22:29:54 +0300

    The main idea of using Repository Pattern in a Laravel application is to create a bridge between models and controllers. In other words, to decouple the hard dependencies of models from the controllers. The model should not be responsible for communicating with or extracting data from the database. This package create interface and repistory implment that interface and bind them in repistoryservice provider and regiser the repositoryserviceprovdier in config/app.php in providers array

---
+++ src/Console/Commands/MakeRepository.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace ZakariaElkashef\RepositoryPattern\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class MakeRepository extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:repository {className?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new repository class';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+                
+        $className = $this->argument('className');
+
+        if (!$className) {
+            $className = $this->ask('What should the repository be named?');
+        }
+    
+        if (!$className) {
+            $this->error('Repository name is required.');
+            return;
+        }
+    
+
+        $interfacePath = app_path('Http/Interfaces/' . $className . 'Interface.php');
+        $repositoryPath = app_path('Http/Repository/' . $className . '.php');
+
+        if (file_exists($interfacePath)) {
+            $this->error("Interface already exists.");
+            return;
+        }
+
+        if (file_exists($repositoryPath)) {
+            $this->error("Repository already exists.");
+            return;
+        }
+
+        $interfaceNamespace = str_replace('/', '\\', 'App\Http\Interfaces');
+        $repositoryNamespace = str_replace('/', '\\', 'App\Http\Repository');
+
+        $interfaceTemplate = "<?php\n\nnamespace $interfaceNamespace;\n\ninterface {$className}Interface\n{\n    // Interface methods...\n}\n";
+        $repositoryTemplate = "<?php\n\nnamespace $repositoryNamespace;\n\nuse $interfaceNamespace\\{$className}Interface;\n\nclass {$className} implements {$className}Interface\n{\n    // Repository methods...\n}\n";
+
+
+        $interfaceDirectory = dirname($interfacePath);
+        $repositoryDirectory = dirname($repositoryPath);
+
+        if (!is_dir($interfaceDirectory)) {
+            mkdir($interfaceDirectory, 0755, true);
+        }
+
+        if (!is_dir($repositoryDirectory)) {
+            mkdir($repositoryDirectory, 0755, true);
+        }
+
+        file_put_contents($interfacePath, $interfaceTemplate);
+        file_put_contents($repositoryPath, $repositoryTemplate);
+
+
+        
+        $repositoryServiceProviderPath = app_path('Providers/RepositoryServiceProvider.php');
+
+        $repositoryServiceProviderNamespace = str_replace('/', '\\', 'App\Providers');
+
+        // Check if the RepositoryServiceProvider already exists
+        if (file_exists($repositoryServiceProviderPath)) {
+            $repositoryServiceProviderContents = file_get_contents($repositoryServiceProviderPath);
+
+            // Check if the binding already exists in the RepositoryServiceProvider
+            if (strpos($repositoryServiceProviderContents, "{$className}Interface::class") === false) {
+                // Add the binding to the existing RepositoryServiceProvider
+                $bindingCode = "        \$this->app->bind('App\Http\Interfaces\\{$className}Interface', 'App\Http\Repository\\{$className}');";
+
+                $repositoryServiceProviderContents = str_replace(
+                    "public function register()\n    {",
+                    "public function register()\n    {\n        $bindingCode",
+                    $repositoryServiceProviderContents
+                );
+
+                file_put_contents($repositoryServiceProviderPath, $repositoryServiceProviderContents);
+
+                $this->info("Interface and Repository bound in the existing RepositoryServiceProvider.");
+            } else {
+                $this->error("Interface and Repository already bound in the RepositoryServiceProvider.");
+            }
+        } else {
+            // Create the RepositoryServiceProvider
+            $repositoryServiceProviderTemplate = "<?php\n\nnamespace $repositoryServiceProviderNamespace;\n\nuse Illuminate\Support\ServiceProvider;\n\nclass RepositoryServiceProvider extends ServiceProvider\n{\n    public function register()\n    {\n        \$this->app->bind('App\Http\Interfaces\\{$className}Interface', 'App\Http\Repository\\{$className}');\n    }\n}";
+
+            file_put_contents($repositoryServiceProviderPath, $repositoryServiceProviderTemplate);
+
+            $this->info("RepositoryServiceProvider created successfully.");
+
+            
+
+            // Add the RepositoryServiceProvider to the config/app.php file
+            $appConfigPath = config_path('app.php');
+            $appConfigContent = file_get_contents($appConfigPath);
+
+            if (strpos($appConfigContent, 'App\Providers\RepositoryServiceProvider::class') === false) {
+                $providerLine = "        App\Providers\RepositoryServiceProvider::class,";
+
+                // Locate the position to add the RepositoryServiceProvider
+                $providersPosition = strpos($appConfigContent, 'ServiceProvider::defaultProviders()->merge([');
+                if ($providersPosition !== false) {
+                    $providersPosition += strlen('ServiceProvider::defaultProviders()->merge([');
+
+                    $appConfigContent = substr_replace($appConfigContent, "\n$providerLine", $providersPosition, 0);
+                }
+
+                file_put_contents($appConfigPath, $appConfigContent);
+
+                $this->info("RepositoryServiceProvider added to config/app.php successfully.");
+            } else {
+                $this->error("RepositoryServiceProvider is already present in config/app.php.");
+            }
+
+            // Bind the interface and repository in the RepositoryServiceProvider
+            $repositoryServiceProviderPath = app_path('Providers/RepositoryServiceProvider.php');
+
+            $repositoryServiceProviderNamespace = str_replace('/', '\\', 'App\Providers');
+
+            // Check if the RepositoryServiceProvider already exists
+            if (file_exists($repositoryServiceProviderPath)) {
+                $repositoryServiceProviderContents = file_get_contents($repositoryServiceProviderPath);
+
+                // Check if the binding already exists in the RepositoryServiceProvider
+                if (strpos($repositoryServiceProviderContents, "{$className}Interface::class") === false) {
+                    // Add the binding to the existing RepositoryServiceProvider
+                    $bindingCode = "        \$this->app->bind('App\Http\Interfaces\\{$className}Interface', 'App\Http\Repository\\{$className}');";
+
+                    $repositoryServiceProviderContents = str_replace(
+                        "public function register()\n    {",
+                        "public function register()\n    {\n        $bindingCode",
+                        $repositoryServiceProviderContents
+                    );
+
+                    file_put_contents($repositoryServiceProviderPath, $repositoryServiceProviderContents);
+
+                    $this->info("Interface and Repository Successfully Bind in the RepositoryServiceProvider.");
+                } else {
+                    $this->error("Interface and Repository already bound in the RepositoryServiceProvider.");
+                }
+            } else {
+                $this->error("RepositoryServiceProvider does not exist. Please run the command to create it.");
+            }
+            
+        }
+
+        $this->info("Interface and Repository created successfully.");
+    }
+}
+
