01c6c8959a7b99d10434e8261881e33f30102ff6
DevZakariaElkashef <dev.zakariaelkashef@gmail.com>
2023-08-25 23:30:00 +0300

    modify service provider

--- src/RepositoryPatternServiceProvider.php
+++ src/RepositoryPatternServiceProvider.php
@@ -3,22 +3,22 @@
 namespace ZakariaElkashef\RepositoryPattern;
 
 use Illuminate\Support\ServiceProvider;
-use ZakariaElkashef\RepositoryPattern\Commands\MakeRepository;
+
 
 class RepositoryPatternServiceProvider extends ServiceProvider
 {
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                \ZakariaElkashef\RepositoryPattern\Commands\MakeRepository::class,
+            ]);
+        }
+    }
     
     public function register()
     {
         
     }
     
-    public function boot()
-    {
-        if ($this->app->runningInConsole()) {
-            $this->commands([
-                MakeRepository::class,
-            ]);
-        }
-    }
 }
